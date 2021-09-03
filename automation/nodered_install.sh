9bc6da33ec908a00e5db6d45d139332d60b994b1
furasesa <angga.furasesa@gmail.com>
2021-09-03 18:30:47 +0700

    add node-red installation

---
+++ automation/nodered_install.sh
@@ -0,0 +1,6 @@
+#/bin/bash
+
+bash <(curl -sL https://raw.githubusercontent.com/node-red/linux-installers/master/deb/update-nodejs-and-nodered) --help
+echo "try:"
+echo "bash <(curl -sL https://raw.githubusercontent.com/node-red/linux-installers/master/deb/update-nodejs-and-nodered) --node14"
+
